8658f5ba1a301181b27e6627363d1d025af186bc
gluck <gluck59@gmail.com>
2025-02-11 13:54:25 +0500

    prettyDump

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-//    phpinfo(); die;
 error_reporting(E_ERROR);
 
 //    $clientInfo = file_get_contents('https://api.hackertarget.com/ipgeo/?q='.$_SERVER['REMOTE_ADDR']);
@@ -8,41 +7,38 @@ error_reporting(E_ERROR);
     if ($_POST['email'] OR $_POST['phone']) {
         try {
             // ТП рекомендует это: https://www.namecheap.com/support/knowledgebase/article.aspx/10038/31/how-to-configure-a-contact-form-with-us/#smtp
+            // это запасной вариант, отправляет через системного юзера kimloltb, нужно для подгрузки DKIM-SPF
             // попробуем если понадобится
 
-            // запасной вариант
-            // отправляет через системного юзера kimloltb, нужно для подгрузки DKIM-SPF
-//            require_once "Mail.php";
-//
-//            $from = "kimloltb@kimlab.link";
-//            $to = "gluck59@gmail.com";
-//            $subject = "Kimlab — контактная форма";
-//
-//            $message = '<p>Сообщение из формы "Оставить заявку" от ' . $_POST['workpost'] . ' ' . $_POST['name'] . '</p>';
-//            $message .= '<p>Email: ' . $_POST['email'] . '</p>';
-//            $message .= '<p>Телефон: ' . $_POST['phone'] . '</p>';
-//            if (!empty($_POST['company'])) $message .= '<p>Компания: ' . $_POST['company'] . '</p>';
-//            if (!empty($_POST['indusrty'])) $message .= '<p>Индустрия: ' . $_POST['indusrty'] . '</p>';
-//            if (!empty($_POST['message'])) $message .= '<p>Сообщение: ' . $_POST['message'] . '</p>';
-//
-//            $host = "kimlab.link";
-//            $username = "kimloltb";
-//            $password = "zkJ17SpbaNCw";
-//
-//            $headers = array('From' => $from, 'To' => $to, 'Subject' => $subject, 'MIME-Version' => 'MIME-Version: 1.0', 'Content-type' => 'text/html; charset=UTF-8');
-//
-//            $smtp = Mail::factory('smtp',
-//                array('host' => $host,
-//                    'auth' => true,
-//                    'username' => $username,
-//                    'password' => $password));
-//            $mail = $smtp->send($to, $headers, $message);
-//            if (PEAR::isError($mail)) {
-//                echo($mail->getMessage());
-//            } else {
-//                $isEmailSent = true;
-//            }
-//
+            /*require_once "Mail.php";
+            $from = "kimloltb@kimlab.link";
+            $to = "gluck59@gmail.com";
+            $subject = "Kimlab — контактная форма";
+
+            $message = '<p>Сообщение из формы "Оставить заявку" от ' . $_POST['workpost'] . ' ' . $_POST['name'] . '</p>';
+            $message .= '<p>Email: ' . $_POST['email'] . '</p>';
+            $message .= '<p>Телефон: ' . $_POST['phone'] . '</p>';
+            if (!empty($_POST['company'])) $message .= '<p>Компания: ' . $_POST['company'] . '</p>';
+            if (!empty($_POST['indusrty'])) $message .= '<p>Индустрия: ' . $_POST['indusrty'] . '</p>';
+            if (!empty($_POST['message'])) $message .= '<p>Сообщение: ' . $_POST['message'] . '</p>';
+
+            $host = "kimlab.link";
+            $username = "kimloltb";
+            $password = "zkJ17SpbaNCw";
+
+            $headers = array('From' => $from, 'To' => $to, 'Subject' => $subject, 'MIME-Version' => 'MIME-Version: 1.0', 'Content-type' => 'text/html; charset=UTF-8');
+
+            $smtp = Mail::factory('smtp',
+                array('host' => $host,
+                    'auth' => true,
+                    'username' => $username,
+                    'password' => $password));
+            $mail = $smtp->send($to, $headers, $message);
+            if (PEAR::isError($mail)) {
+                echo($mail->getMessage());
+            } else {
+                $isEmailSent = true;
+            } */
 
 
             $headers = 'From: Kimlab' . "\r\n" .
@@ -66,22 +62,44 @@ error_reporting(E_ERROR);
         } catch (Exception $e) {
             throw new Exception('Send Email Error: ',  $e->getMessage(), 400);
         };
+    } // if POST
+
+
+    // установка языка стр
+    if (isset($_GET['lang']) AND $_GET['lang'] != '') $sLang = $_GET['lang'];
+    else $sLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    //echo '$sLang='.$sLang;//.' — $isEmailSent='.$isEmailSent;
+    if (!include strtolower($sLang).'.php') include 'en.php';
+
+
+
+
+
+/**
+ * выводит print_r / var_dump по-человечьи
+ *
+ * @param $data
+ * @param $die
+ * @param $showStack
+ * @return void
+ */
+function prettyDump($data = null, $die = false, $showStack = false) {
+//	if (in_array($_SERVER['SERVER_ADDR'], ['127.0.0.1', '::1', '0.0.0.0', 'localhost']))
+    {
+        $stack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        echo "<pre style='text-align: left;font-size: 14px;font-family: Courier, monospace; background-color: #f4f4f4; width: fit-content; opacity: .9; z-index: 999;position: relative;margin: 0 0 0 300px;'>";
+        if ($showStack) print_r($stack);
+        if ($stack[0]['function'] == 'prettyDump') {
+            echo __FUNCTION__.'() из '.$stack[0]['file'].' line '.$stack[0]['line'].'<br>';
+        } else {
+//			print_r($stack);
+            echo __FUNCTION__.'() из '.($stack[1]['args'][0] ? $stack[1]['args'][0] : $stack[2]['file']).' строка '.$stack[0]['line'].':<br>';
+        }
+        if (is_bool($data) || is_null($data) || empty($data)) var_dump($data);
+        else print_r($data);
+        echo "</pre>";
+        if ($die) die;
     }
-
-if (isset($_GET['lang']) AND $_GET['lang'] != '') $sLang = $_GET['lang'];
-else $sLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-//echo '$sLang='.$sLang;//.' — $isEmailSent='.$isEmailSent;
-if (!include strtolower($sLang).'.php') include 'en.php';
-
-
-//////////////////////////////////////////////////////
-    function printr($input = null, $die = false) {
-    $html = '<pre>';
-    $html .= print_r($input, 1);
-    $html .= '</pre>';
-
-    echo $html;
-    if ($die) die;
 }
 
 
